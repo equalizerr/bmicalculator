b8115f94d7b9481afddd31f3a230df71653b725c
equalizerr <59252676+equalizerr@users.noreply.github.com>
2020-11-01 21:49:27 +0100

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,9 +1,9 @@
 bmi = int
 weight = input("Your weight in kilogram: ")
-weight = float(weight)
+weight = float(weight) # Converting weight to a float
 size = input("Your Size in meters: ")
-size = float(size)
-size = size ** 2
-bmi = weight / size
-bmi = round(bmi, 1)
-print("Your BMI is", bmi)
+size = float(size) # Conerting size to a float
+size = size ** 2 # squaring size by 2
+bmi = weight / size # bmi = weight / size^2
+bmi = round(bmi, 1) # Round the bmi by 2 after decimal point
+print("Your BMI is", bmi) # Printing out your BMI
